0f0ca606da4f3a8567e858035ec4fd8bec0e3ca5
bakirov99 <btimur.mail@gmail.com>
2024-07-18 21:01:15 +0500

    Update app.py
    commit new class

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 
+
+class Car:
+
+  def __init__(self, make: str, color: str) -> None:
+    self.make = make
+    self.color = color
+
+  def __str__(self) -> str:
+    return f"make: {self.make}\ncolor: {self.color}"
+
+car1 = Car('chevrolet', 'white')
